c3bc7ac3a38d42bdf6ce4b6ba989098c68c09054
Bilal2558 <137030303+Bilal2558@users.noreply.github.com>
2023-09-22 18:29:09 +0500

    Initial commit

---
+++ aap.js
@@ -0,0 +1,44 @@
+// 1
+
+alert("error please enter a valid password")
+
+// 2
+
+let str = "Hello World!\nThis is my string";
+
+alert(str);
+
+// 3
+
+
+let abc = "Hello World!\nThis is my string";
+
+console .log(abc);
+
+// 4
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+
+// 5
+var anx = " Thanks   input! "
+alert(anx);
+
+// 6
+let br = " PLZZA\n PLZZ\n PLLZ\n PLL\n PL\n P "
+alert(br);
+
+// 7
+let email = " e.g. example@example.com "
+alert(email);
+
+// 8
+let book = " A smarter way to learn JavaScript"
+alert(book);
+
+// 9
+let boder = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(boder);
+
+
+
