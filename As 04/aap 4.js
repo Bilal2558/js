89b0bfdf5eed0e5e7d1200a92eeaebdf690dd357
Bilal2558 <137030303+Bilal2558@users.noreply.github.com>
2023-09-30 18:31:44 +0500

    gg

--- As 04/aap 4.js
+++ As 04/aap 4.js
@@ -1,8 +1,8 @@
 
 
-let a = 5;        // Statement 2
-let b = 6;        // Statement 3
-let c = 9;        // Statement 3
+let a = 5;        // Statement 5
+let b = 6;        // Statement 6
+let c = 9;        // Statement 9
 
 
 alert(a+b+c)
