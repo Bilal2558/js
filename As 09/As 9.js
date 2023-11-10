3b9f7212479d27fa126e13e55b2f0ae75df49859
Bilal2558 <137030303+Bilal2558@users.noreply.github.com>
2023-11-10 19:17:37 +0500

    10

--- As 09/As 9.js
+++ As 09/As 9.js
@@ -1,7 +1,67 @@
 document.write("<h1>...........", "As #13-15</h1>");
 // 1
-document.write("<h3>...........1<br>array using  student names in future<h3>");
+document.write("<br><h3>...........1literalArray<h3>");
+var literalArray = [];
 
-var student = ["Aftab", "Bilal", "Sajid", "Naveed"];
-var pets[3]="atif";
-document.write(student);
+// 2
+document.write("<br><h3>...........2objectArray<h3>");
+var objectArray = new Array();
+
+// 3
+document.write("<br><h3>...........3strings array<h3>");
+var stringArray= ["This ", "is ", "String ", "Array"];
+
+// 4
+document.write("<br><h3>...........4numbers array<h3>");
+var numberArray= [1,2,3,4,5];
+
+// 5
+document.write("<br><h3>...........5boolean array<h3>");
+var boolArray=[true, false];
+
+// 6
+document.write("<br><h3>...........6mixed array<h3>");
+var mixedArray= ["Pakistan: ", 1947];
+
+// 7
+document.write("<br><h3>...........7education qualifications in  array<h3>");
+var education= ["Qualification: <br> <br>"+"1) SSC<br>"+ "2) HSC<br>"+ "3) BCS<br>"+ "4) BS<br>"+ "5) BCOM<br>"+ "6) MS<br>"+ "7) M. Phil<br>"+ "8) PhD<br>"];
+// document.write(education);
+
+// 8
+document.write("<br><h3>...........8 students  result in  array<h3>");
+var studentNames = ["Ali", "Bilal", "Danish"];
+var totalScore= 500;
+var score= [490, 480, 470];
+for(var i=0; i<score.length;i++){
+    // document.write("<br> Score of " + studentNames[i] + " is " + score[i] + ". Percentage: " + score[i]*100/totalScore + "% <br>");
+}
+
+// 9
+document.write("<br><h3>...........9  color name in  array wrking<h3>");
+
+
+// 10
+document.write("<br><h3>...........10  student scores & sort the array in ascending order using Array’s sort method.<h3><br>");
+var scores= [320, 230, 480, 120];
+// document.write("====>Scores of Students: " +scores +  "<br>");
+// document.write("====>Ordered Scores of Students: " + scores.sort());
+
+// 11
+document.write("<br><h3>...........11  working<h3><br>");
+
+// 12
+document.write("<br><h3>...........12join method in array<h3><br>");
+var arr=["This ", "is ", "my ", "cat "];
+
+// document.write("Array: <br>" + arr + "<br><br>");
+// var singleString = arr.join(" ");
+// document.write("String: <br>" + singleString );
+
+// 13
+document.write("<br><h3>...........13  values one by one<h3><br>");
+var devices = ["keyboard", "mouse", "printer", "monitor"];
+document.write(devices.shift() + "<br>");
+document.write(devices.shift() + "<br>");
+document.write(devices.shift() + "<br>");
+document.write(devices.shift() + "<br>");
--- aap.js
+++ aap.js
@@ -1,15 +1,46 @@
 
+// array
+document.write("<h3>........... array<h3>");
+var pets = ["A ","B ", "C ", "D ","F "];
+document.write("<h6>=====>",pets ,"<h6>");
 
+// Order in array
+document.write("<br><h3>...........Order in array<h3>");
+ pets[1] = "cat ";
+ pets[2] = "bird ";
+ document.write("<h6>=====>",pets ,"<h6>");
 
- document.write("<h3>...........Pets use to add nuber start<h3>");
-var pets = [1,2, 5, 5,8];
-pets[0] = "dog";
- pets[1] = "cat";
- pets[2] = "bird";
- document.write("=====>",pets, "<br>");
+//  Pop use to remove end word
+ document.write("<br><h3>........... Pop use to remove end  word <h3>");
+ pets.pop();
+ document.write("<h6>=====>",pets,"<br></h6>");
 
- document.write("<h3>........... Pets use to add nuber end<h3>");
- pets.push("fish", "ferret","<br>");
- document.write("=====>",pets,"<br>");
-
+//  Push use to add end  word
+ document.write("<br><h3>........... Pets use to add end  word <h3>");
+ pets.push("fish","<br>");
+ document.write("<h6>=====>",pets,"</h6>");
  
+//  Shift use to remove start  word
+document.write("<br><h3>...........  Shift use to remove start  word <h3>");
+ pets.shift();
+ document.write("<h6>=====>",pets,"<br></h6>");
+
+ //  Unshift use to add start word
+document.write("<h3>...........  Unshift  use to add start word <h3>");
+pets.unshift("fish  ");
+document.write("<h6>=====>",pets,"<br></h6>");
+
+ //  Splice use to add and remove in array
+ document.write("<h3>...........  Splice use to  remove in array<h3>");
+pets.splice(2, 1, "pig");
+document.write("<h6>=====>",pets,"<br></h6>");
+
+ //  Splice use to add  in array
+ document.write("<h3>...........  Splice use to add  in array<h3>");
+pets.splice(2, 0, "foc");
+document.write("<h6>=====>",pets,"<br></h6>");
+
+ //  Slice use to copy  in array
+ document.write("<h3>........... Slice use to copy  in array<h3>");
+ var noPets = pets.slice(1, 3);
+document.write("<h6>=====>",noPets,"<br></h6>");
